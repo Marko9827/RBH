7dde8b7e496efa19b1ce7739009b6a9f1ae2012c
Marko Nikolic <marko.supergun@gmail.com>
2023-06-13 22:39:41 +0200

    sh startup script

---
+++ first_run.sh
@@ -0,0 +1,7 @@
+# _-- Don't move this file or edit!
+npm install -g @angular/cli
+echo "1/5 Angular installed"
+npm install
+echo "2/3 node_modules: instaled"
+ng serve
+echo "3/3 Angular project started on: http://localhost:4200/"
