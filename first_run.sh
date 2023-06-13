0f0fdbdc3849f76d814f740a022e5205fa07553e
Marko Nikolic <marko.supergun@gmail.com>
2023-06-13 22:46:53 +0200

    test

--- first_run.sh
+++ first_run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # _-- Don't move this file or edit!
 npm install -g @angular/cli
 echo "1/5 Angular installed"
@@ -5,3 +7,4 @@ npm install
 echo "2/3 node_modules: instaled"
 ng serve
 echo "3/3 Angular project started on: http://localhost:4200/"
+start http://localhost:4200/"
